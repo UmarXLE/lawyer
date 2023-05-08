47a0ad3e097dd0fc5bb530ecd21171c9cc5b45a6
UmarXLE <umar.xle@gmail.com>
2023-05-08 20:52:17 +0600

    create/aboutPage50%

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
+import ServicesPage from "./pages/ServicesPage/ServicesPage";
 function App() {
   return (
     <div className="App">
       <Routes>
           <Route path='/' element={<HomePage/>}/>
           <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/service' element={<ServicesPage/>}/>
+
       </Routes>
     </div>
   );
