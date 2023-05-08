2c0ce59dafd780fd3243028226f2aaba40b241fd
UmarXLE <umar.xle@gmail.com>
2023-05-08 18:56:43 +0600

    createFooter

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import css from './Footer.module.scss'
+import logo from '../../media/icons/footer-logo.png'
+import instagrammIcon from '../../media/icons/inst.png'
+import whatsAppIcon from '../../media/icons/whats.png'
+import telegramIcon from '../../media/icons/tg.png'
+const Footer = () => {
+    return (
+        <footer className={css.footer}>
+            <div className={css.footer__logo}>
+                <img src={logo} alt=""/>
+                <p>Алгоритм Прайм - Порядок имеет значение</p>
+            </div>
+            <div className={css.footer__social}>
+                <p>Социальные сети</p>
+                <div className={css.footer__social_container}>
+                    <img src={instagrammIcon} alt=""/>
+                    <img src={whatsAppIcon} alt=""/>
+                    <img src={telegramIcon} alt=""/>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
