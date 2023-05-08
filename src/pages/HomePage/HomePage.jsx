33dc8c77260ace3fca26f0426ccfa918c9adf3f1
UmarXLE <umar.xle@gmail.com>
2023-05-08 16:54:59 +0600

    1

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import css from './HomePage.module.scss'
+const HomePage = () => {
+    return (
+        <div className={css.wrapper}>
+
+        </div>
+    );
+};
+
+export default HomePage;
