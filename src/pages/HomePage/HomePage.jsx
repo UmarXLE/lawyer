6f04c0dd51c0ad06ffb4f04bcb612c8ded59961d
UmarXLE <umar.xle@gmail.com>
2023-05-11 17:50:13 +0600

    added/stylesForAccordion

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,12 +4,14 @@ import Header from "../../components/Header/Header";
 import Intro from "../../components/Intro/Intro";
 import ServicesBlog from "../../shared/feature/ServicesBlog/ServicesBlog";
 import Footer from "../../components/Footer/Footer";
+import AccordionItem from "../../components/Accordion/AccordionItem";
 const HomePage = () => {
     return (
         <div className={css.wrapper}>
             <Header />
             <Intro />
             <ServicesBlog />
+            <AccordionItem />
             <Footer/>
         </div>
     );
