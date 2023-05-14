64d1d534fb371a32688b77e727207cc4ba4dea13
UmarXLE <mlssaitov@gmail.com>
2023-05-14 21:31:30 +0300

    setting/accordionData

--- src/components/AccordionWrapper/AccordionWrapper.jsx
+++ src/components/AccordionWrapper/AccordionWrapper.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import css from './AccordinoWrapper.module.scss'
-import {accordion} from "../../api/data";
+import {accordion , accordionData2 , accordionData3} from "../../api/data";
 import AccordionItem from "../Accordion/AccordionItem";
 const AccordionWrapper = () => {
     console.log(accordion)
     return (
         <section className={css.wrapper}>
-            <AccordionItem props={accordion}/>
+            <AccordionItem data={accordion}/>
+            <AccordionItem data={accordionData2}/>
+            <AccordionItem data={accordionData3}/>
+
         </section>
     );
 };
